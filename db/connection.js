c569693a792f40fcc0b70cd9f3de19d9dc7990b7
Vinayak-Sannaik <vinayakpsannaik@gmail.com>
2024-12-14 19:53:03 +0530

    refactoring

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Vinayak@#32000',
-  database: 'postal_locator',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10,  
+  connectionLimit: 10,
   queueLimit: 0,
 });
 
@@ -15,14 +15,14 @@ const promisePool = pool.promise();
 
 const getFavorites = async () => {
   try {
-    const [rows] = await promisePool.query('SELECT * FROM favorites');
+    const [rows] = await promisePool.query("SELECT * FROM favorites");
     return rows;
   } catch (error) {
-    throw new Error('Failed to fetch favorites: ' + error.message);
+    throw new Error("Failed to fetch favorites: " + error.message);
   }
 };
 
 module.exports = {
   promisePool,
-  getFavorites
+  getFavorites,
 };
